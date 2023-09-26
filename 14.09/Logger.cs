f52f7e631810bb67157e3142849ff972c6cec78a
nonam00 <raisky.ilya3@yandex.ru>
2023-09-26 18:33:25 +0300

    commit

---
+++ 14.09/Logger.cs
@@ -0,0 +1,28 @@
+﻿namespace _14._09;
+
+public class Logger
+{
+    private string path;
+    private string equation = String.Empty;
+
+    public Logger() => path = "D:\\Райский\\14.09\\14.09\\TextFile1.txt";
+
+    public Logger(string fileName) =>  path = fileName;
+
+    public void Operation(int a, int b)
+    {
+        equation = $"{a} / {b} = ";
+
+        try { equation += $"{a / b}"; }
+        catch (Exception ex) { Except(ex); }
+    }
+    public override string ToString() => equation;
+
+    public void Except(Exception ex) => equation += ex.Message;
+
+    public void Logging()
+    {
+        using StreamWriter sw = new(path, true);
+        sw.WriteLine(equation);
+    }
+}
---
+++ 14.09/Program.cs
@@ -0,0 +1,6 @@
+﻿using _14._09;
+
+Logger logger = new();
+logger.Operation(2, 1);
+Console.WriteLine(logger);
+logger.Logging();
